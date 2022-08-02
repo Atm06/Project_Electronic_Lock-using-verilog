a7094fc832b689b8ca87e580dbd4c49e032120d9
Ashutosh Mallick <78423220+Atm06@users.noreply.github.com>
2022-08-02 10:02:28 +0530

    first commit

--- testbench.sv
+++ testbench.sv
@@ -1,32 +1,27 @@
-////////////////TEST BENCH CODE FOR ELECTRONIC LOCK//////////////////
-
-module electronic_lock_tb;
-  reg clk, rst;
-  reg [1:0]b ; 
-  wire unlock;
-  integer   i;
-  reg [15:0]data;
-  
-  electronic_lock dut(clk, rst, b[0], b[1], unlock);
-  always #1 clk = ~clk;
-  
-  initial
-    begin
-      $monitor("time=%0t, b[o]=%b, b[1]=%b, unlock=%b", $time, b[0], b[1], unlock);
-      $dumpfile("output.vcd");
-      $dumpvars(1, electronic_lock_tb);
-      clk = 0;
-      rst = 0;
-      #50  rst =1;
-      data = 16'b1110101101101110;
-      i=0;
-      #400 $finish;
-      end
-  
-  always @ (posedge clk) begin
-    b  = data >> i;
-    i = i+1;
+// Code your testbench here
+// or browse Examples
+module tb;
+  reg clk,in,rstn;
+  wire out;
+  integer i;
+  reg[15:0] data;
+  always #10clk=~clk;
+  det_01011 u0(.clk(clk),.rstn(rstn),.in(in),.out(out));
+  initial begin $monitor("i=%0d,Time=%0d,rstn=%b,in=%b,out=%b",i,$time,rstn,in,out);
+    $dumpfile("tb.vcd");
+    $dumpvars(1,tb);
+    clk=0;
+    rstn=0;
+    #50 
+    rstn=1;
+    data = 16'b0101101011010110;
+    i=0;
+    #400
+    $finish;
+  end
+  always @(posedge clk) begin
+    in=data >>i;
+    i=i+1;
   end
 endmodule
-    
-    
+  
